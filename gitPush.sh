27b7c5b67b05061b916e4cb494bb99cfe05ed62d
“GZONER” <rp170854@gmail.com>
2022-03-27 13:38:33 +0530

    Shell File Updated

--- gitPush.sh
+++ gitPush.sh
@@ -4,6 +4,12 @@ then
 	echo "Enter the commit string"
 else
 	git commit -m "$1"
+	echo -ne '#####                     (33%)\r'
+	sleep 1
+	echo -ne '#############             (66%)\r'
+	sleep 1
+	echo -ne '#######################   (100%)\r'
+	echo -ne '\n'
 	git push
 fi
 exit
